97b8cbc7b2f067d3c64b7aaf2dd159cf6a9688fa
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2018-06-07 19:25:05 +0100

    Generalise pop

--- core/jscomp.ml
+++ core/jscomp.ml
@@ -873,9 +873,10 @@ module SIM_CPS = struct
     val identity : t
     val (<>) : t -> t -> t
 
-  (* Returns a scope in which the head and tail of the continuation
-     are accessible. *)
-    val pop : t -> Js.decl list * t * t
+    (* Pops n elements from a given continuation. Returns a list of
+       declarations, a list of variables, and the tail of the
+       continuation. *)
+    val pop : int -> t -> Js.decl list * t list * t
 
   (* Turns code into a continuation. *)
     val reflect : Js.expression -> t
@@ -962,53 +963,43 @@ module SIM_CPS = struct
              bind bs (fun kappas -> body (Cons (EVar k, kappas))) kappas
         in
         bind [] body kappas
-        (* let rec bind (bs : Js.statement -> Js.program) (ks : t -> t) = *)
-        (*   function *)
-        (*   | Identity -> *)
-        (*      let k = Ident.make ~prefix:"_kappa" () in *)
-        (*      (fun code -> *)
-        (*        let (decls, prog) = bs code in *)
-        (*        (gen_bind k (reify Identity)) :: decls, code), ks, EVar k *)
-        (*   | Reflect ((EVar _) as v) -> *)
-        (*      bs, ks, v *)
-        (*   | Reflect v -> *)
-        (*      let k = Ident.make ~prefix:"_kappa" () in *)
-        (*      (fun code -> let (decls, prog) = bs code in (gen_bind k v) :: decls, prog), ks, EVar k *)
-        (*   | Cons ((EVar _) as v, kappas) -> *)
-        (*      bind bs (fun kappas -> Cons (v, kappas)) kappas *)
-        (*   | Cons (v, kappas) -> *)
-        (*      let k = Ident.make ~prefix:"_kappa" () in *)
-        (*      bind *)
-        (*        (fun code -> let (decls, prog) = bs code in (gen_bind k v code) :: decls, prog) *)
-        (*        (fun kappas -> Cons (EVar k, kappas)) kappas *)
-        (* in *)
-        (* let bs, ks, seed = bind (fun code -> code) (fun kappas -> kappas) kappas in *)
-        (* bs (body (ks (reflect seed))) *)
 
       let apply k arg =
         Js.(EApply (EPrim "%K.apply", [reify k; arg]))
 
-      let rec pop = function
-        | Cons (kappa, kappas) ->
-           [], (reflect kappa), kappas
-        | Reflect ks ->
-           let open Js in
-           let __k = Ident.make ~prefix:"__k" () in
-           let __ks = Ident.make ~prefix:"__ks" () in
-           let __k_binding =
-             DLet {
-               bkind = `Const;
-               binder = __k;
-               expr = head ks; }
-           in
-           let __ks_binding =
-             DLet {
-               bkind = `Const;
-               binder = __ks;
-               expr = tail ks; }
-           in
-           [__k_binding; __ks_binding], (reflect (EVar __k)), reflect (EVar __ks)
-        | Identity -> pop toplevel
+      let pop : int -> t -> Js.decl list * t list * t
+        = fun n kappa ->
+          let rec loop n kappa =
+            let rec pop = function
+              | Cons (kappa, kappas) ->
+                 [], (reflect kappa), kappas
+              | Reflect ks ->
+                 let open Js in
+                 let __k = Ident.make ~prefix:"__k" () in
+                 let __ks = Ident.make ~prefix:"__ks" () in
+                 let __k_binding =
+                   DLet {
+                     bkind = `Const;
+                     binder = __k;
+                     expr = head ks; }
+                 in
+                 let __ks_binding =
+                   DLet {
+                     bkind = `Const;
+                     binder = __ks;
+                     expr = tail ks; }
+                 in
+                 [__k_binding; __ks_binding], (reflect (EVar __k)), reflect (EVar __ks)
+              | Identity -> pop toplevel
+            in
+            if n > 0 then
+              let (decls, hd, tl) = pop kappa in
+              let (decls', ks, tl) = loop (n-1) tl in
+              (decls @ decls', hd :: ks, tl)
+            else
+              ([], [], kappa)
+          in
+          loop n kappa
 
       let contify_with_env fn =
         let open Js in
@@ -1241,14 +1232,13 @@ module SIM_CPS = struct
          let nil = EPrim "%List.nil" in
          K.bind kappa
            (fun kappas ->
-             let decls, skappa, kappas = K.pop kappas in
-             let decls', seta, kappas   = K.pop kappas in
+             let [@warning "-8"] (decls, [skappa; seta], kappas) = K.pop 2 kappas in
              let resumption = K.(cons (reify seta) (cons (reify skappa) nil)) in
              let op    =
                make_dictionary [ ("_label", strlit name)
                                ; ("_value", make_dictionary [("p", box args); ("s", resumption)]) ]
              in
-             decls @ decls', K.(SReturn (apply (seta <> kappas) op)))
+             decls, K.(SReturn (apply (seta <> kappas) op)))
       | `Handle { Ir.ih_comp = comp; Ir.ih_return = return; Ir.ih_cases = eff_cases; Ir.ih_depth = depth } ->
          let open Utility in
          if depth = `Shallow then
@@ -1263,7 +1253,7 @@ module SIM_CPS = struct
            let (xb, body) = return_clause in
            let x_name = snd @@ safe_name_binder xb in
            contify (fun kappa ->
-             let decls, _, kappa = K.pop kappa in
+             let decls, _, kappa = K.pop 1 kappa in
              EFun {
                fname = `Anonymous;
                fkind = `Regular;
@@ -1313,8 +1303,7 @@ module SIM_CPS = struct
            let forward ks _z =
              K.bind ks
                (fun ks ->
-                 let decls, k', ks' = K.pop ks in
-                 let decls', h', ks' = K.pop ks' in
+                 let [@warning "-8"] (decls, [k'; h'], ks') = K.pop 2 ks in
                  let resumption =
                    let s = Ident.of_string "s" in
                    let _x = Ident.make ~prefix:"_x" () in
@@ -1332,7 +1321,7 @@ module SIM_CPS = struct
                    }
                  in
                  let vmap = EApply (EPrim "%K.vmap", [resumption; EVar _z]) in
-                 decls @ decls', (SReturn K.(apply (h' <> ks') vmap)))
+                 decls, (SReturn K.(apply (h' <> ks') vmap)))
            in
            let _z = Ident.of_string "_z" in
            contify
@@ -1369,7 +1358,7 @@ module SIM_CPS = struct
           | `Let (b, (_, tc)) :: bs ->
              let (x, x_name) = safe_name_binder b in
              let x_name = Ident.of_string x_name in
-             let decls, skappa, skappas = K.pop kappa in
+             let (decls, [skappa], skappas) = K.pop 1 kappa in
              let env',skappa' =
                K.contify_with_env
                  (fun kappas ->
